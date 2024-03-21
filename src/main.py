43973119a94ff88313940c897cc1ee0e1df1d2ba
Ky Huynh <hgky95@gmail.com>
2024-03-20 21:02:40 -0400

    integrate open-ai

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
 from flask import Flask
 from flask_restful import Api
-from dotenv import load_dotenv
 
-from src.resources.OpenAI import OpenAI
 
-load_dotenv()
+from src.resources.OpenAIIntegration import OpenAIIntegration
+
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(OpenAI, '/open-ai')
+api.add_resource(OpenAIIntegration, '/open-ai')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/resources/OpenAI.py
+++
@@ -1,5 +0,0 @@
-from flask_restful import Resource
-class OpenAI(Resource):
-    def get(self):
-        return {'hello': 'world'}
-

0fb0c0480fb2fb30059ca29bdac9ed2661c87acf
Ky Huynh <hgky95@gmail.com>
2024-03-31 15:48:32 -0400

    change api endpoint

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from flask import Flask
 from flask_restful import Api
 
-
 from resources.AIIntegration import AIIntegration
-
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(AIIntegration, '/open-ai')
+api.add_resource(AIIntegration, '/ai/query')
 
 if __name__ == '__main__':
     app.run(debug=True)

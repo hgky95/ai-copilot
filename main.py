e13e0c15d1429e3a62bc79af9723e411976f4182
Ky Huynh <hgky95@gmail.com>
2024-04-09 20:33:46 -0400

    remove unused file and code

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ from flask import Flask
 from flask_restful import Api
 
 from resources.AIIntegration import AIIntegration
-from embedchain import App
 
 app = Flask(__name__)
 api = Api(app)
-#embedchain_app = App.from_config(config_path="./config/general_config.yaml")
 
-#api.add_resource(AIIntegration, '/ai/query', resource_class_kwargs={'embedchain_app': embedchain_app})
 api.add_resource(AIIntegration, '/ai/query')
 
 if __name__ == '__main__':
